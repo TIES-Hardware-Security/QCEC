1500ff414ba185b3177d2de2545eee0da3b01818
ssmiftah <87914799+ssmiftah@users.noreply.github.com>
2024-07-11 17:54:50 -0500

    Add files via upload

---
+++ build/src/CMakeFiles/qcec.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/chao/Projects/qcec_final/qcec/src/EquivalenceCheckingManager.cpp" "src/CMakeFiles/qcec.dir/EquivalenceCheckingManager.cpp.o" "gcc" "src/CMakeFiles/qcec.dir/EquivalenceCheckingManager.cpp.o.d"
+  "/Users/chao/Projects/qcec_final/qcec/src/checker/EquivalenceChecker.cpp" "src/CMakeFiles/qcec.dir/checker/EquivalenceChecker.cpp.o" "gcc" "src/CMakeFiles/qcec.dir/checker/EquivalenceChecker.cpp.o.d"
+  "/Users/chao/Projects/qcec_final/qcec/src/checker/dd/DDAlternatingChecker.cpp" "src/CMakeFiles/qcec.dir/checker/dd/DDAlternatingChecker.cpp.o" "gcc" "src/CMakeFiles/qcec.dir/checker/dd/DDAlternatingChecker.cpp.o.d"
+  "/Users/chao/Projects/qcec_final/qcec/src/checker/dd/DDConstructionChecker.cpp" "src/CMakeFiles/qcec.dir/checker/dd/DDConstructionChecker.cpp.o" "gcc" "src/CMakeFiles/qcec.dir/checker/dd/DDConstructionChecker.cpp.o.d"
+  "/Users/chao/Projects/qcec_final/qcec/src/checker/dd/DDEquivalenceChecker.cpp" "src/CMakeFiles/qcec.dir/checker/dd/DDEquivalenceChecker.cpp.o" "gcc" "src/CMakeFiles/qcec.dir/checker/dd/DDEquivalenceChecker.cpp.o.d"
+  "/Users/chao/Projects/qcec_final/qcec/src/checker/dd/DDSimulationChecker.cpp" "src/CMakeFiles/qcec.dir/checker/dd/DDSimulationChecker.cpp.o" "gcc" "src/CMakeFiles/qcec.dir/checker/dd/DDSimulationChecker.cpp.o.d"
+  "/Users/chao/Projects/qcec_final/qcec/src/checker/dd/applicationscheme/GateCostApplicationScheme.cpp" "src/CMakeFiles/qcec.dir/checker/dd/applicationscheme/GateCostApplicationScheme.cpp.o" "gcc" "src/CMakeFiles/qcec.dir/checker/dd/applicationscheme/GateCostApplicationScheme.cpp.o.d"
+  "/Users/chao/Projects/qcec_final/qcec/src/checker/zx/ZXChecker.cpp" "src/CMakeFiles/qcec.dir/checker/zx/ZXChecker.cpp.o" "gcc" "src/CMakeFiles/qcec.dir/checker/zx/ZXChecker.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/chao/Projects/qcec_final/qcec/build/src/extern/qfr/src/CMakeFiles/qfr_zx.dir/DependInfo.cmake"
+  "/Users/chao/Projects/qcec_final/qcec/build/src/extern/qfr/src/CMakeFiles/qfr.dir/DependInfo.cmake"
+  "/Users/chao/Projects/qcec_final/qcec/build/src/extern/qfr/src/extern/zx/src/CMakeFiles/zx.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/qcec.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qcec.dir/EquivalenceCheckingManager.cpp.o"
+  "CMakeFiles/qcec.dir/EquivalenceCheckingManager.cpp.o.d"
+  "CMakeFiles/qcec.dir/checker/EquivalenceChecker.cpp.o"
+  "CMakeFiles/qcec.dir/checker/EquivalenceChecker.cpp.o.d"
+  "CMakeFiles/qcec.dir/checker/dd/DDAlternatingChecker.cpp.o"
+  "CMakeFiles/qcec.dir/checker/dd/DDAlternatingChecker.cpp.o.d"
+  "CMakeFiles/qcec.dir/checker/dd/DDConstructionChecker.cpp.o"
+  "CMakeFiles/qcec.dir/checker/dd/DDConstructionChecker.cpp.o.d"
+  "CMakeFiles/qcec.dir/checker/dd/DDEquivalenceChecker.cpp.o"
+  "CMakeFiles/qcec.dir/checker/dd/DDEquivalenceChecker.cpp.o.d"
+  "CMakeFiles/qcec.dir/checker/dd/DDSimulationChecker.cpp.o"
+  "CMakeFiles/qcec.dir/checker/dd/DDSimulationChecker.cpp.o.d"
+  "CMakeFiles/qcec.dir/checker/dd/applicationscheme/GateCostApplicationScheme.cpp.o"
+  "CMakeFiles/qcec.dir/checker/dd/applicationscheme/GateCostApplicationScheme.cpp.o.d"
+  "CMakeFiles/qcec.dir/checker/zx/ZXChecker.cpp.o"
+  "CMakeFiles/qcec.dir/checker/zx/ZXChecker.cpp.o.d"
+  "libqcec.a"
+  "libqcec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qcec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
