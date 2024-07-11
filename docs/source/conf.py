2055374e977604db49effeb09ed69c9e2ad8dc27
ssmiftah <87914799+ssmiftah@users.noreply.github.com>
2024-07-11 17:51:00 -0500

    Add files via upload

---
+++ docs/source/conf.py
@@ -0,0 +1,88 @@
+import sys
+
+if sys.version_info < (3, 10, 0):
+    import importlib_metadata as metadata
+else:
+    from importlib import metadata
+
+import pybtex.plugin
+from pybtex.style.formatting.unsrt import Style as UnsrtStyle
+from pybtex.style.template import field, href
+
+# -- Project information -----------------------------------------------------
+project = "QCEC"
+author = "Lukas Burgholzer"
+
+release = metadata.version("mqt.qcec")
+version = ".".join(release.split(".")[:3])
+language = "en"
+copyright = "Chair for Design Automation, Technical University of Munich"
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinxcontrib.bibtex",
+    "sphinx_copybutton",
+    "hoverxref.extension",
+    "nbsphinx",
+    "sphinxext.opengraph",
+    "sphinx_rtd_dark_mode",
+]
+
+nbsphinx_execute = "auto"
+highlight_language = "python3"
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+    "--InlineBackend.rc=figure.dpi=200",
+]
+nbsphinx_kernel_name = "python3"
+
+autosectionlabel_prefix_document = True
+
+hoverxref_auto_ref = True
+hoverxref_domains = ["cite", "py"]
+hoverxref_roles = []
+hoverxref_mathjax = True
+hoverxref_role_types = {
+    "ref": "tooltip",
+    "p": "tooltip",
+    "labelpar": "tooltip",
+    "class": "tooltip",
+    "meth": "tooltip",
+    "func": "tooltip",
+    "attr": "tooltip",
+    "property": "tooltip",
+}
+exclude_patterns = ["_build", "build", "**.ipynb_checkpoints", "Thumbs.db", ".DS_Store", ".env"]
+
+
+class CDAStyle(UnsrtStyle):
+    def format_url(self, e):
+        url = field("url", raw=True)
+        return href()[url, "[PDF]"]
+
+
+pybtex.plugin.register_plugin("pybtex.style.formatting", "cda_style", CDAStyle)
+
+bibtex_bibfiles = ["refs.bib"]
+bibtex_default_style = "cda_style"
+
+copybutton_prompt_text = r"(?:\(venv\) )?(?:\[.*\] )?\$ "
+copybutton_prompt_is_regexp = True
+copybutton_line_continuation_character = "\\"
+
+autosummary_generate = True
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = "sphinx_rtd_theme"
+html_baseurl = "https://qcec.readthedocs.io/en/latest/"
+html_logo = "_static/mqt_light.png"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
---
+++ docs/source/index.rst
@@ -0,0 +1,51 @@
+Welcome to QCEC's documentation!
+================================
+
+QCEC is a tool for :doc:`quantum circuit equivalence checking <EquivalenceChecking>` developed as part of the *Munich Quantum Toolkit* (*MQT*) [#]_ by the `Chair for Design Automation <https://www.cda.cit.tum.de/>`_ at the `Technical University of Munich <https://www.tum.de>`_. It builds upon `our quantum functionality representation (QFR) <https://github.com/cda-tum/qfr>`_, `our decision diagram (DD) package <https://github.com/cda-tum/dd_package.git>`_, and `our ZX-diagram package <https://github.com/cda-tum/zx.git>`_.
+
+We recommend you to start with the :doc:`installation instructions <Installation>`.
+Then proceed to the :doc:`quickstart guide <Quickstart>` and read the :doc:`reference documentation <library/Library>`.
+If you are interested in the theory behind QCEC, have a look at the publications in the :doc:`publication list <Publications>`.
+
+We appreciate any feedback and contributions to the project. If you want to contribute, you can find more information in the :doc:`Contribution <Contributing>` guide. If you are having trouble with the installation or the usage of QCEC, please let us know at our :doc:`Support <Support>` page or by reaching out to us at `quantum.cda@xcit.tum.de <mailto:quantum.cda@xcit.tum.de>`_.
+
+----
+
+ .. toctree::
+    :hidden:
+
+    self
+
+ .. toctree::
+    :maxdepth: 2
+    :caption: User Guide
+    :glob:
+
+    Installation
+    Quickstart
+    EquivalenceChecking
+    CompilationFlowVerification
+    ParameterizedCircuits
+    Publications
+
+ .. toctree::
+    :maxdepth: 2
+    :caption: Developers
+    :glob:
+
+    Contributing
+    DevelopmentGuide
+    Support
+
+ .. toctree::
+    :maxdepth: 6
+    :caption: API Reference
+    :glob:
+
+    library/Library
+
+----
+
+.. rubric:: Footnotes
+
+.. [#] The Munich Quantum Toolkit was formerly known under the acronym *JKQ* :cite:labelpar:`wille2020JKQtools` and developed by the `Institute for Integrated Circuits <https://iic.jku.at/eda/>`_ at the `Johannes Kepler University Linz <https://jku.at>`_.
